47abb2565feaa5f0d164d8ae3fd9392616342dd9
leesaxby <lee.saxby82@gmail.com>
2021-10-24 19:30:28 +0100

    Break card into separate component and reuse on players page

--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,17 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import Layout from '../components/layout';
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import {blue, green, red, grey} from "@mui/material/colors";
-import SmileIcon from "@mui/icons-material/Mood";
-import ListItemText from "@mui/material/ListItemText";
-import SadIcon from "@mui/icons-material/MoodBad";
-import WinRate from "@mui/icons-material/TrendingDown";
-import { CardActions, CardHeader, CardMedia } from "@mui/material";
+import DisplayCard from '../components/displayCard';
+
 
 const Decks = () => {
     const [decks, setDecks] = useState([]);
@@ -33,69 +24,15 @@ const Decks = () => {
                 alignItems="center"
                 spacing={4}>
                 {
-                    decks.map(({ ref, data}) => {
+                    decks.map(({ ref, data: { name, commander, imageURL }}) => {
                         return (
                             <Grid
                                 item
                                 key={ref['@ref'].id}>
-                                <Card sx={{ maxWidth: 345 }}>
-                                    <CardHeader
-                                        avatar={
-                                            <Avatar sx={{ bgcolor: grey[500] }} aria-label="Commander" />
-                                        }
-                                        title={data.commander}
-                                        subheader="Lee Saxby"
-                                    />
-                                    <CardMedia
-                                        component="img"
-                                        height="194"
-                                        image={data.imageURL}
-                                        alt="Paella dish"
-                                    />
-                                    <CardActions disableSpacing>
-                                                <List
-                                                    dense
-                                                    sx={{
-                                                        display: 'flex',
-                                                        flexDirection: 'row',
-                                                        padding: 0,
-                                                        marginTop: 1,
-                                                        marginBottom: 1,
-                                                    }}>
-                                                    <ListItem >
-                                                        <ListItemAvatar>
-                                                            <Avatar sx={{ bgcolor: green[500] }}>
-                                                                <SmileIcon />
-                                                            </Avatar>
-                                                        </ListItemAvatar>
-                                                        <ListItemText
-                                                            primary={5}
-                                                        />
-
-                                                    </ListItem>
-                                                    <ListItem>
-                                                        <ListItemAvatar>
-                                                            <Avatar sx={{ bgcolor: red[500] }}>
-                                                                <SadIcon />
-                                                            </Avatar>
-                                                        </ListItemAvatar>
-                                                        <ListItemText
-                                                            primary={5}
-                                                        />
-                                                    </ListItem>
-                                                    <ListItem >
-                                                        <ListItemAvatar>
-                                                            <Avatar sx={{ bgcolor: blue[500] }}>
-                                                                <WinRate />
-                                                            </Avatar>
-                                                        </ListItemAvatar>
-                                                        <ListItemText
-                                                            primary="45%"
-                                                        />
-                                                    </ListItem>
-                                                </List>
-                                    </CardActions>
-                                </Card>
+                                <DisplayCard
+                                    name={name}
+                                    commander={commander}
+                                    imageURL={imageURL} />
                             </Grid>
                         )
                     })
