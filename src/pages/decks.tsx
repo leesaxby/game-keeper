3a279853ea99d3f32fa57a166caa2cce7844a99b
Lee <lee.saxby82@gmail.com>
2021-12-13 14:10:43 +0000

    Add basic create deck functionality

--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,11 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import Layout from '../components/layout';
 import Grid from "@mui/material/Grid";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import DisplayCard from '../components/displayCard';
 
 
 const Decks = () => {
     const [decks, setDecks] = useState([]);
+    const [createDeckOpen, setCreateDeckOpen] = useState(false);
 
     console.log(decks)
     useEffect(() => {
@@ -17,28 +25,58 @@ const Decks = () => {
     }, [])
 
     return (
-        <Layout>
-            <Grid
-                container
-                justifyContent="center"
-                alignItems="center"
-                spacing={4}>
-                {
-                    decks.map(({ ref, data: { name, commander, imageURL }}) => {
-                        return (
-                            <Grid
-                                item
-                                key={ref['@ref'].id}>
-                                <DisplayCard
-                                    name={name}
-                                    commander={commander}
-                                    imageURL={imageURL} />
-                            </Grid>
-                        )
-                    })
-                }
-            </Grid>
-        </Layout>
+        <>
+            <Layout
+                actions={(
+                    <Button
+                        variant="outlined"
+                        onClick={() => setCreateDeckOpen(true)}>ADD DECK</Button>
+                )}
+                main={(
+                    <Grid
+                        container
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={4}>
+                        {
+                            decks.map(({ ref, data: { name, commander, imageURL }}) => {
+                                return (
+                                    <Grid
+                                        item
+                                        key={ref['@ref'].id}>
+                                        <DisplayCard
+                                            name={name}
+                                            commander={commander}
+                                            imageURL={imageURL} />
+                                    </Grid>
+                                )
+                            })
+                        }
+                    </Grid>
+                )} />
+            <Dialog open={createDeckOpen}>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                <DialogContentText>
+                    To subscribe to this website, please enter your email address here. We
+                    will send updates occasionally.
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setCreateDeckOpen(false)}>Cancel</Button>
+                    <Button onClick={() => {}}>Subscribe</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
