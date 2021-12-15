9dfa0e6ad008a8ed8d829ba562d3eb1b9080ee91
leesaxby <lee.saxby82@gmail.com>
2021-12-15 12:01:23 +0000

    Setup results

---
+++ functions/games.js
@@ -0,0 +1,46 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+})
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'GET'){
+        return { statusCode: 500, body: 'GET OUTTA HERE!' }
+    }
+
+    try {
+        const req = await client.query(
+            q.Map(
+                q.Paginate(q.Match(q.Index("all_games"))),
+                q.Lambda("gameRef",
+                    q.Let({
+                            gameDoc: q.Get(q.Var("gameRef")),
+                        },
+                        {
+                            id: q.Select(["ref", "id"], q.Var("gameDoc")),
+                            winner: q.Get(q.Select(['data', 'winner'], q.Var('gameDoc'))),
+                            winMethod: q.Select(['data', 'winMethod'], q.Var('gameDoc')),
+                            first: q.Select(["data", "first"], q.Var("gameDoc")),
+                            turnOneSolRing: q.Select(["data", "turnOneSolRing"], q.Var("gameDoc")),
+                            losers: q.Map(q.Select(['data', 'losers'], q.Var('gameDoc')), q.Lambda(['deckRef'],
+                                q.Let({
+                                        item: q.Get(q.Var('deckRef')),
+                                    },
+                                    {
+                                        name: q.Select(['data', 'name'], q.Var('item')),
+                                        player: q.Select(['data', 'name'],q.Get(q.Select(['data', 'player'], q.Var('item')))),
+                                    }
+                                ))),
+                        }
+                    )
+                )
+            )
+        )
+
+        return { statusCode: 200, body: JSON.stringify(req.data) }
+    } catch (err) {
+        return { statusCode: 500, body: JSON.stringify({ error: err.message}) }
+    }
+}
--- src/pages/results.js
+++ src/pages/results.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Layout from '../components/layout';
 
 const Results = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        fetch('/.netlify/functions/games')
+            .then(res => res.json())
+            .then(res => setGames(res))
+            .catch(err => console.log(err))
+    }, [])
 
     return (
-        <Layout>
-           Results
-        </Layout>
+        <>
+            <Layout
+                main={(
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Winner</TableCell>
+                                    <TableCell>Method</TableCell>
+                                    <TableCell>Went First</TableCell>
+                                    <TableCell>Turn One Sol Ring</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {games.map(({ id, winner, winMethod, first, turnOneSolRing }) => (
+                                    <TableRow
+                                        key={id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                        <TableCell>{winner.data.name}</TableCell>
+                                        <TableCell>{winMethod}</TableCell>
+                                        <TableCell>{first ? 'Yes' : 'No'}</TableCell>
+                                        <TableCell>{turnOneSolRing ? 'Yes' : 'No'}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )} />
+        </>
     )
 }
 
